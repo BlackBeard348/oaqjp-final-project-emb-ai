f601665cf279403b1665c2b091da652d3ffd4030
BlackBeard348 <cmorgan348@outlook.com>
2025-07-27 05:45:20 -0400

    Task 3 save

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,22 @@
 import requests  # Import the requests library to handle HTTP requests
+import json
 
 def emotion_detector(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
     myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+
+     # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+     # Extracting sentiment label and score from the response
+    anger_score = formatted_response['emotionPredictions']['anger']
+    disgust_score = formatted_response['emotionPredictions']['disgust']
+    fear_score = formatted_response['emotionPredictions']['fear']
+    joy_score = formatted_response['emotionPredictions']['joy']
+    sadness_score = formatted_response['emotionPredictions']['sadness']
+    dominant_emotion = formatted_response['emotionPredictions']['<name of the dominant emotion>']
+
+
     return response.text  # Return the response text from the API
